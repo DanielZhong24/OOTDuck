7575780faec955ce1fb6a5ef138e48215e8425ad
DanielZhong24 <danielzhong0618@gmail.com>
2025-09-08 23:03:57 -0400

    Installed icons and pwa plugins

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,5 +1,8 @@
 import { Link } from "react-router-dom";
-
+import HomeIcon from '~icons/mdi-light/home';
+import CameraIcon from '~icons/mdi-light/camera';
+import BookmarkIcon from '~icons/mdi-light/bookmark';
+import SettingIcon from '~icons/mdi-light/settings';
 function Navbar() {
   return (
     <div className="fixed bottom-0 left-0 w-full bg-black p-5">
@@ -10,7 +13,7 @@ function Navbar() {
             className="flex items-end justify-center border-b-2 border-transparent text-center hover:border-gray-500"
           >
             <span className="block px-1">
-              <i className="far fa-home pt1 mb-1 block text-xl text-white transition-colors duration-200 hover:text-gray-500"></i>
+              <HomeIcon className="pt1 mb-1 block text-xl text-white transition-colors duration-200 hover:text-gray-500" />
             </span>
           </Link>
         </div>
@@ -20,7 +23,7 @@ function Navbar() {
             className="flex items-end justify-center border-b-2 border-transparent text-center hover:border-gray-500"
           >
             <span className="block px-1">
-              <i className="far fa-camera-retro pt1 mb-1 block text-xl text-white transition-colors duration-200 hover:text-gray-500"></i>
+              <CameraIcon className="far fa-camera-retro pt1 mb-1 block text-xl text-white transition-colors duration-200 hover:text-gray-500"/>
             </span>
           </Link>
         </div>
@@ -30,7 +33,7 @@ function Navbar() {
             className="flex items-end justify-center border-b-2 border-transparent text-center hover:border-gray-500"
           >
             <span className="block px-1">
-              <i className="far fa-grin-hearts pt1 mb-1 block text-xl text-white transition-colors duration-200 hover:text-gray-500"></i>
+              <BookmarkIcon className="far fa-grin-hearts pt1 mb-1 block text-xl text-white transition-colors duration-200 hover:text-gray-500"/>
             </span>
           </Link>
         </div>
@@ -40,7 +43,7 @@ function Navbar() {
             className="flex items-end justify-center border-b-2 border-transparent text-center hover:border-gray-500"
           >
             <span className="block px-1">
-              <i className="far fa-user-circle pt1 mb-1 block text-xl text-white transition-colors duration-200 hover:text-gray-500"></i>
+              <SettingIcon className="far fa-user-circle pt1 mb-1 block text-xl text-white transition-colors duration-200 hover:text-gray-500"/>
             </span>
           </Link>
         </div>
---
+++ frontend/src/icons.d.ts
@@ -0,0 +1,6 @@
+
+declare module '~icons/*' {
+  import * as React from 'react'
+  const component: React.ComponentType<React.SVGProps<SVGSVGElement>>
+  export default component
+}
