b8447af61b3c546209deb01a12e265a51e08a663
DanielZhong24 <danielzhong0618@gmail.com>
2025-09-10 01:39:10 -0400

    finished manifest settings
    richer pwa ui doesnt work on ios we are cooked

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import Icons from 'unplugin-icons/vite';
@@ -7,6 +7,7 @@ import {VitePWA} from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
+
   plugins: [
     react(), 
     tailwindcss(),
@@ -20,6 +21,7 @@ export default defineConfig({
         name: "Dressify",
         short_name: "Dressify",
         start_url: "/",
+        id:"/",
         display: "standalone",
         background_color: "#ffffff",
         icons: [
@@ -33,18 +35,28 @@ export default defineConfig({
             sizes: "512x512",
             type: "image/png",
           }
-        ]},
+        ],
+        "screenshots": [
+          {
+            "src": "src/assets/sc.png",
+            "sizes": "1400x900",
+            "type": "image/png",
+            "form_factor": "wide",
+          },
+          {
+            "src": "src/assets/512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          }
+        ]
+      },
     })
   ],
-  root:'../frontend',
   server: {
-    proxy: {
-      '/api': {
-        // target: ,
-        changeOrigin: true, 
-      },
-    },
+    allowedHosts:true
   }
+  
+
 
 });
 
