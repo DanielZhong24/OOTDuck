96e7417a0b4202bed1b41ce226d1215dafe098b7
DanielZhong24 <danielzhong0618@gmail.com>
2025-09-26 16:21:38 -0400

    removed randomize class

--- backend/src/routes/randomize.route.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from 'express';
-import clothesRandomizeController from '../controllers/clothesRandomize.controller.js';
-
-const router: Router = Router();
-
-router.get('/match/:id', clothesRandomizeController.randomizeMatchingColour);
